d9a724a062bd4c1e730d975d2d0ff98a72d3c964
Uwe Riegel <uriegel@hotmail.de>
2022-11-03 21:00:55 +0100

    getString

--- Gtk4RustFs/Program.fs
+++ Gtk4RustFs/Program.fs
@@ -1,10 +1,21 @@
 ﻿open System.Runtime.InteropServices
 
-[<DllImport("../../../../target/debug/librust.so", SetLastError = true, CharSet=CharSet.Auto)>]
-extern int add_numbers(int a, int b)
+[<DllImport("../../../../target/debug/librust.so", SetLastError = true, EntryPoint="add_numbers")>]
+extern int addNumbers(int a, int b)
+[<DllImport("../../../../target/debug/librust.so", SetLastError = true, EntryPoint="get_name")>]
+extern nativeint getName()
+[<DllImport ("libc.so.6")>]
+ extern void free(nativeint ptr)
+
+let fromPtr (ptr: nativeint) = 
+    let str = Marshal.PtrToStringUTF8 ptr
+    free ptr
+    str
 
 printfn "Hello from F#"
-printfn "addNumbers: %d" <| add_numbers (12, 99)
+printfn "addNumbers: %d" <| addNumbers (12, 99)
+printfn "getString: %s" <| (fromPtr <| getName ())
+
 
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+use std::slice::from_raw_parts_mut;
+
+use libc::malloc;
+
+unsafe fn get_string_ptr(str: &str) -> *mut u8 {
+    let bytes = str.as_bytes();
+    let len = bytes.len();
+    let ptr = malloc(len + 1) as *mut u8;
+    let slice0 = from_raw_parts_mut(ptr, len + 1);        
+    slice0[len] = 0;
+    let slice = from_raw_parts_mut(ptr, len);        
+    slice.copy_from_slice(bytes);
+    ptr
+}
+
+
 #[no_mangle]
 pub extern fn add_numbers(a: i32, b: i32) -> i32 {
     println!("Hello from rust!");
     a + b
+}
+
+#[no_mangle]
+pub extern fn get_name() -> *mut u8 {
+    let string = "Hello from Rust😂😂😂😂";
+    unsafe {
+        get_string_ptr(string)
+    }
 }
